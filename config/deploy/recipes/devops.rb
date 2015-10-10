32c6d835eed90bace0a4efce05af43b58941ced7
Assaf Shomer <assafshomer@gmail.com>
2015-10-10 15:32:36 +0300

    gemrc setup

--- config/deploy/recipes/devops.rb
+++ config/deploy/recipes/devops.rb
@@ -1,11 +1,18 @@
 namespace :devops do
-   desc "Copy files"
-   task :copy do
-      on roles(:all) do |host|
-				set :rails_root, File.expand_path("../../../../", __FILE__)+'/'
-				%w(.env config/database.yml).each do |f|
-				  upload! fetch(:rails_root).to_s+ f , shared_path + f
-				end
-      end
-   end
+	desc "Copy files"
+	task :copy do
+		on roles(:all) do |host|
+			set :rails_root, File.expand_path("../../../../", __FILE__)+'/'
+			%w(.env config/database.yml).each do |f|
+				upload! fetch(:rails_root).to_s+ f , shared_path + f
+			end
+		end
+	end
+
+	task :nodoc do
+		on roles(:all) do
+			execute "echo 'gem: --no-document' > ~/.gemrc"
+			execute "echo 'gem: --no-ri --no-rdoc' >> ~/.gemrc"
+		end
+	end
 end
