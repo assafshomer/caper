77da5fa05eec11f2bbe5b39068b80452e4edf01b
Assaf Shomer <assafshomer@gmail.com>
2015-10-22 01:07:44 +0300

    deploy works now, but nothing is actually served

--- config/deploy/recipes/unicorn.rb
+++ config/deploy/recipes/unicorn.rb
@@ -1,5 +1,5 @@
 set_if_empty(:unicorn_user, fetch(:deploy_user))
-set_if_empty(:unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid")
+set_if_empty(:unicorn_pid, "#{shared_path}/config/unicorn.pid")
 set_if_empty(:unicorn_config, "#{shared_path}/config/unicorn.rb")
 set_if_empty(:unicorn_log, "#{shared_path}/log/unicorn.log")
 set_if_empty(:unicorn_workers, 2)
@@ -8,13 +8,15 @@ namespace :unicorn do
   desc "Setup Unicorn initializer and app configuration"
   task :setup do
     on roles(:app) do
-      execute :sudo, "mkdir -p #{fetch(:shared_path)}/config"
+      execute :sudo, "mkdir -p #{shared_path}/config"
+      execute :sudo, "mkdir -p #{shared_path}/log"
+      execute :sudo, "chmod -R 777 #{shared_path}/log"
       template "unicorn.rb.erb", "/tmp/unicorn.rb"
       execute :sudo, "mv /tmp/unicorn.rb #{fetch(:unicorn_config)}"
       template "unicorn_init.erb", "/tmp/unicorn_init"
       execute :sudo, "chmod +x /tmp/unicorn_init"
       execute :sudo, "mv /tmp/unicorn_init /etc/init.d/unicorn_#{fetch(:application)}"
-      execute :sudo, "update-rc.d -f unicorn_#{fetch(:application)} defaults"     
+      execute :sudo, "update-rc.d -f unicorn_#{fetch(:application)} defaults"
     end
   end
 
@@ -24,7 +26,7 @@ namespace :unicorn do
       on roles(:app) do
         within current_path do
           puts capture(:env)
-          execute "service unicorn_#{fetch(:application)} #{command}"
+          execute :sudo, "service unicorn_#{fetch(:application)} #{command}"
         end
       end      
     end    
